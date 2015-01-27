be6e63df4c7830f775d417632a7f170159015188
Tim Golen <tgolen@appcelerator.com>
2015-01-26 20:41:44 -0700

    added the model loader to the package
    Signed-off-by: Tim Golen <tgolen@appcelerator.com>

---
+++ loader.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var glob = require('glob'),
+	path = require('path'),
+	_ = require('lodash'),
+	swagger = require('swagger-node-restify'),
+	swaggerMethodName = {
+		get: 'addGet',
+		put: 'addPut',
+		post: 'addPost',
+		delete: 'addDelete'
+	},
+	modelPath = null;
+	models = [];
+
+/**
+ * gets all model files and adds them to swagger
+ * it will then add those to swagger as the proper resource
+ *
+ * @author Tim Golen 2014-12-17
+ *
+ * @param  {Function} cb
+ */
+function init(pathToModels, cb) {
+	modelPath = pathToModels;
+	glob(modelPath + '/**/*.js', function(err, files) {
+		if (err) return cb && cb(err);
+
+		_(files).forEach(function(file) {
+			var Model = require(file)(),
+				modelSwagger = Model.getSwagger();
+			if (modelSwagger) {
+				_(modelSwagger).each(function(swaggerResource) {
+					swagger[swaggerMethodName[swaggerResource.spec.method.toLowerCase()]](swaggerResource);
+				});
+			}
+			models.push(Model);
+		});
+
+		return cb && cb(null, models);
+	});
+}
+
+/**
+ * returns all of our model files
+ *
+ * @author Tim Golen 2015-01-26
+ *
+ * @param  {Function} cb
+ *
+ * @return {array} an array containing all of our models
+ */
+function models(cb) {
+	return cb(null, models);
+}
+
+exports.init = init;
+exports.models = models;
